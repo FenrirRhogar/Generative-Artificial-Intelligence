0e4f35f9fb503c8b5b9cb1c2853825c22383f1e1
FenrirRhogar <103751641+FenrirRhogar@users.noreply.github.com>
2025-08-06 08:54:50 +0300

    test files removed

--- Project 2/main.py
+++
@@ -1,8 +0,0 @@
-import torch
-def main():
-    print("PyTorch Version:", torch.__version__)
-    print("CUDA Version:", torch.version.cuda)
-    print("Is CUDA available?", torch.cuda.is_available())
-
-if __name__ == "__main__":
-    main()
